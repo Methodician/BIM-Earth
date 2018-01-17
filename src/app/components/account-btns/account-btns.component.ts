76fa6c543dc08d59b931ee436963aacd2cb6b21f
Joe Parsley <joewparsley@gmail.com>
2018-01-16 17:27:05 -0800

    install button and menu modules from angular material, create new account-btns component

---
+++ src/app/components/account-btns/account-btns.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bim-account-btns',
+  templateUrl: './account-btns.component.html',
+  styleUrls: ['./account-btns.component.scss']
+})
+export class AccountBtnsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
