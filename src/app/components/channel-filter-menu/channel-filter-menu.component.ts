1309343cdf458414b2cc0ead45d02ad0bd0a3c33
Aubrey Masten <mastenaub@gmail.com>
2018-01-27 16:49:26 -0800

    add channel filtering menu

---
+++ src/app/components/channel-filter-menu/channel-filter-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bim-channel-filter-menu',
+  templateUrl: './channel-filter-menu.component.html',
+  styleUrls: ['./channel-filter-menu.component.scss']
+})
+export class ChannelFilterMenuComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+}
