28ac566c7dd384f52634bbd69638114d6620da98
Aubrey Masten <mastenaub@gmail.com>
2018-02-20 08:51:33 -0800

    add image and file display to posts

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostComponent implements OnInit {
   @Input() post;
+  images = [];
+  imagesLoaded = false;
+  files = [];
+  filesLoaded = false;
+  headerImage;
 
   get postDate() {
     if (this.post.timestamp) return `${this.post.timestamp.getMonth()+1}/${this.post.timestamp.getDay()}/${this.post.timestamp.getYear() - 100} at ${this.post.timestamp.getHours()}:${this.post.timestamp.getMinutes()}`;
@@ -25,8 +30,26 @@ export class PostComponent implements OnInit {
     return this.post.author;
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if(!this.post.images.length) {
+      this.images = this.getArrayfromObject("images");
+      this.headerImage = this.images.shift();
+      this.imagesLoaded = true;
+    }
+    if(!this.post.files.length) {
+      this.files = this.getArrayfromObject("files");
+      this.filesLoaded = true;
+    }
+  }
+
+  getArrayfromObject(fileType: "images" | "files") {
+    let fileArray = [];
+    for(let key in this.post[fileType]) {
+      fileArray.push(this.post[fileType][key]);
+    }
+    return fileArray;
+  }
 
 }
